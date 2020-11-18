1166dbe081de2cb02990cd2619de445ba25bfe9b
neo <neochne@gmail.com>
2020-11-18 18:08:01 +0800

    remove redundant imports

--- src/pattern/Test.java
+++ src/pattern/Test.java
@@ -1,16 +1,12 @@
 package pattern;
 
-import pattern.creational.singleton.lazy.LazySingleton;
-
-import java.util.List;
-
 /**
  * Created by sharp on 2018/8/23.
  */
 public class Test {
 
     public static void main(String[] args) {
-
+        
     }
 
 }
